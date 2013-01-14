d2ec5082fe106ee1633291e004f9f1cb40b29611
Robert Lemke <robert@typo3.org>
2013-01-14 16:17:55 +0100

    [FEATURE] blog:create-post command

--- Classes/RobertLemke/Plugin/Blog/Command/BlogCommandController.php
+++ Classes/RobertLemke/Plugin/Blog/Command/BlogCommandController.php
@@ -52,6 +52,67 @@ class BlogCommandController extends \TYPO3\Flow\Cli\CommandController {
 	protected $contentTypeManager;
 
 	/**
+	 * Create a new blog post
+	 *
+	 * This command creates a new and empty blog post in the specified workspace.
+	 *
+	 * @param string $blogNodePath Absolute node path of the parent node which contains the blog.
+	 * @param string $workspace Name of the workspace to create the blog posts in.
+	 * @return void
+	 */
+	public function createPostCommand($blogNodePath, $workspace) {
+		$shortcutContentType = $this->contentTypeManager->getContentType('TYPO3.Neos.ContentTypes:Shortcut');
+		$sectionContentType = $this->contentTypeManager->getContentType('TYPO3.Neos.ContentTypes:Section');
+		$textContentType = $this->contentTypeManager->getContentType('TYPO3.Neos.ContentTypes:Text');
+		$postContentType = $this->contentTypeManager->getContentType('RobertLemke.Plugin.Blog:Post');
+
+		$contentContext = new ContentContext($workspace);
+		$this->nodeRepository->setContext($contentContext);
+
+		$blogBaseNode = $contentContext->getNode($blogNodePath);
+		if ($blogBaseNode === NULL) {
+			$this->outputLine('The blog base node "%s" does not exist - you need to create a page with that path manually.', array($blogNodePath));
+			$this->quit(1);
+		}
+
+		$postsBaseNode = $blogBaseNode->getNode('posts');
+
+		$slug = uniqid('post');
+		$date = new \DateTime();
+
+		$yearNode = $postsBaseNode->getNode($date->format('Y'));
+		if ($yearNode === NULL) {
+			$yearNode = $postsBaseNode->createNode($date->format('Y'), $shortcutContentType);
+			$yearNode->setProperty('title', $date->format('Y'));
+		}
+
+		$monthNode = $yearNode->getNode($date->format('m'));
+		if ($monthNode === NULL) {
+			$monthNode = $yearNode->createNode($date->format('m'), $shortcutContentType);
+			$monthNode->setProperty('title', $date->format('m'));
+		}
+
+		$dayNode = $monthNode->getNode($date->format('d'));
+		if ($dayNode === NULL) {
+			$dayNode = $monthNode->createNode($date->format('d'), $shortcutContentType);
+			$dayNode->setProperty('title', $date->format('d'));
+		}
+
+		$postNode = $dayNode->createNode($slug, $postContentType);
+		$postNode->setProperty('title', 'New blog post');
+		$postNode->setProperty('datePublished', $date);
+		$postNode->setProperty('category', '');
+		$postNode->setProperty('tags', '');
+
+		$contentSectionNode = $postNode->createNode('main', $sectionContentType);
+
+		$textNode = $contentSectionNode->createNode(uniqid('text'), $textContentType);
+		$textNode->setProperty('text', 'Create some content here.');
+
+		$this->outputLine('Created new blog post at %s.', array($postNode->getPath()));
+	}
+
+	/**
 	 * Migrate to Neos-based blog
 	 *
 	 * This command migrates an existing blog based on the standalone blog app to
@@ -169,7 +230,6 @@ class BlogCommandController extends \TYPO3\Flow\Cli\CommandController {
 				$this->outputLine('   Create comment node "%s"', array($commentNode->getPath()));
 			}
 		}
-
 	}
 
 }
