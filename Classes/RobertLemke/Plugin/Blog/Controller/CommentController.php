751f23e3f7b4a3c69164d4037c16098b0979012c
Robert Lemke <rl@robertlemke.com>
2013-03-27 17:28:31 +0100

    [BUGFIX] Fix validation for submitted comments
    This adds a preliminary validation of newly submitted comments
    and fixes the date / time of these comments.

--- Classes/RobertLemke/Plugin/Blog/Controller/CommentController.php
+++ Classes/RobertLemke/Plugin/Blog/Controller/CommentController.php
@@ -55,8 +55,23 @@ class CommentController extends ActionController {
 	 * @return void
 	 */
 	public function createAction(PersistentNodeInterface $postNode, NodeTemplate $newComment) {
+			# Workaround until we can validate node templates properly:
+		if (strlen($newComment->getProperty('author')) < 2) {
+			$this->addFlashMessage('Your comment was NOT created - you need to specify your name.');
+			$this->redirect('show', 'Frontend\Node', 'TYPO3.Neos', array('node' => $postNode));
+		}
+		if (strlen($newComment->getProperty('text')) < 5) {
+			$this->addFlashMessage('Your comment was NOT created - it was too short.');
+			$this->redirect('show', 'Frontend\Node', 'TYPO3.Neos', array('node' => $postNode));
+		}
+		if (filter_var($newComment->getProperty('emailAddress'), FILTER_VALIDATE_EMAIL) === FALSE) {
+			$this->addFlashMessage('Your comment was NOT created - you must specify a valid email address.');
+			$this->redirect('show', 'Frontend\Node', 'TYPO3.Neos', array('node' => $postNode));
+		}
+
 		$commentNode = $postNode->getNode('comments')->createNodeFromTemplate($newComment, uniqid('comment-'));
 		$commentNode->setProperty('spam', FALSE);
+		$commentNode->setProperty('datePublished', new \DateTime());
 
 		if ($this->akismetService->isCommentSpam('', $commentNode->getProperty('text'), 'comment', $commentNode->getProperty('author'), $commentNode->getProperty('emailAddress'))) {
 			$commentNode->setProperty('spam', TRUE);
