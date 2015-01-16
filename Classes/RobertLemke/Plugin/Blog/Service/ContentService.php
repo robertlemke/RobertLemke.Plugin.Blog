4334f96f34c94dd9ce022d7d1e4ba236e7caa338
Karsten Dambekalns <karsten@dambekalns.de>
2015-01-16 13:19:05 +0100

    [TASK] Fix wrong license in file docblocks

--- Classes/RobertLemke/Plugin/Blog/Service/ContentService.php
+++ Classes/RobertLemke/Plugin/Blog/Service/ContentService.php
@@ -5,8 +5,7 @@ namespace RobertLemke\Plugin\Blog\Service;
  * This script belongs to the TYPO3 Flow package "RobertLemke.Plugin.Blog" *
  *                                                                         *
  * It is free software; you can redistribute it and/or modify it under     *
- * the terms of the GNU General Public License, either version 3 of the    *
- * License, or (at your option) any later version.                         *
+ * the terms of the MIT License.                                           *
  *                                                                         *
  * The TYPO3 project - inspiring people to share!                          *
  *                                                                         */
